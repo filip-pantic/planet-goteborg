b98d36db3e1c5c521c6b660c01d9b327f6c8a163
Filip Pantic <filip-pantic@outlook.com>
2023-11-16 21:55:49 +0100

    Setup fetch for API

--- js/main.js
+++ js/main.js
@@ -1,29 +1,57 @@
-var baseUrl = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/";
+const baseUrl = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com";
+let requestKey = "";
+let planetAllData = null;
 
-const listOfPlanets = [
-  "Sun",
-  "Mercury",
-  "Venus",
-  "Earth",
-  "Mars",
-  "Jupiter",
-  "Saturn",
-  "Uranus",
-  "Neptune",
-  "Pluto",
-];
+const getCredentials = function () {
+  var myHeaders = new Headers();
+  myHeaders.append("x-zocom", "solaris-1Cqgm3S6nlMechWO");
 
-// API requests
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(baseUrl + "/keys", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      requestKey = result.key;
+      getPlanetDetails();
+    })
+    .catch((error) => console.log("error", error));
+};
+
+const getPlanetDetails = function (event) {
+  myHeaders.append("x-zocom", "solaris-KwOi5vm2TYNmi8Dd");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(baseUrl + "/bodies", requestOptions)
+    .then((response) => response.json())
+    .then((result) => planetAllData = result.bodies)
+    .catch((error) => console.log("error", error));
+};
+
+const showPlanetDetails = () => {
+  console.log('planetAllData', planetAllData)
+}
+
 var main = function () {
-  var allPlanets = document.querySelectorAll(".planet");
+  // Get key and Details
+  getCredentials();
 
-  
+  // Planet click
+  const allPlanets = document.querySelectorAll(".planet");
 
   for (let i = 0; i < allPlanets.length; i++) {
-    document.addEventListener("click", getPlanetDetails);
+    document.addEventListener("click", showPlanetDetails);
   }
-
-  console.log("filip nasao:", allPlanets);
 };
+
+// event.target?.querySelector("span.planet-description")?.textContent?.toLowerCase()
 
 document.addEventListener("DOMContentLoaded", main, false);
