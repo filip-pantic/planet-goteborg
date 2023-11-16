caf860371c393422b348ebb9b50a7094ae952eb1
Filip Pantic <filip-pantic@outlook.com>
2023-11-16 21:19:09 +0100

    Loop planets

--- js/main.js
+++ js/main.js
@@ -13,8 +13,15 @@ const listOfPlanets = [
 
 // API requests
 var main = function () {
+  var allPlanets = document.querySelectorAll(".planet");
 
-  console.log("filip");
+  
+
+  for (let i = 0; i < allPlanets.length; i++) {
+    document.addEventListener("click", getPlanetDetails);
+  }
+
+  console.log("filip nasao:", allPlanets);
 };
 
 document.addEventListener("DOMContentLoaded", main, false);
